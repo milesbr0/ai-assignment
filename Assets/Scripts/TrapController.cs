57e923dd92986fbd69cce77f931062de2c3da562
milesbr0 <17065178@students.southwales.ac.uk>
2019-12-27 17:01:04 +0100

    2d game

---
+++ Assets/Scripts/TrapController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapController : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 1.0f;
+    private Transform player;
+    private Transform respawnPoint;
+    private ParticleSystem particleSystem;
+
+    private void Start()
+    {
+        particleSystem = gameObject.GetComponent<ParticleSystem>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        respawnPoint = GameObject.FindGameObjectWithTag("RespawnPoint").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        float rotationZ = gameObject.transform.rotation.z;
+        rotationZ += rotationSpeed * Time.deltaTime;
+        gameObject.transform.Rotate(gameObject.transform.rotation.x, gameObject.transform.rotation.y, rotationZ);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        player.transform.position = respawnPoint.transform.position;
+        particleSystem.Play();
+    }
+}
---
+++ Assets/TwoDCharacterController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoDCharacterController : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed = 1.0f;
+    [SerializeField] private float jumpHeight = 2.0f;
+    Rigidbody2D rigidbody2D;
+
+
+    bool isJumping;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = gameObject.transform.position.x;
+        float horizontal = gameObject.transform.position.y;
+
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            vertical += moveSpeed ;
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            vertical -= moveSpeed;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow) && isJumping == false)
+        {
+            rigidbody2D.AddForce(new Vector2(0, jumpHeight),ForceMode2D.Impulse);
+           // isJumping = true;
+        }
+
+
+
+        gameObject.transform.position = new Vector2(vertical, horizontal);
+    }
+}
