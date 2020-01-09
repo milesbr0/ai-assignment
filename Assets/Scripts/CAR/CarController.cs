72478aabb6de74388b9a708eb00a08fa486c0665
milesbr0 <17065178@students.southwales.ac.uk>
2020-01-09 21:17:13 +0000

    final project

---
+++ Assets/Scripts/CAR/CarController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    Rigidbody rigidBody; //rigidbody of the car
+    [SerializeField] private float verticalSpeed;
+    [SerializeField] private float horizontalSpeed;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move(Input.GetAxisRaw("Vertical"), Input.GetAxisRaw("Horizontal"));
+    }
+
+    private void Move(float vertical, float horizontal)
+    {
+        rigidBody.velocity = transform.right * vertical * verticalSpeed;
+        rigidBody.angularVelocity = transform.up * horizontal * horizontalSpeed;
+    }
+}
