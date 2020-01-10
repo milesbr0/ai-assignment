7659ad3372df7ceef962d3db0e68a755714bd8cc
milesbr0 <17065178@students.southwales.ac.uk>
2020-01-10 01:41:01 +0000

    commit b4 sleep

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -71,11 +71,9 @@ public class Manager : MonoBehaviour
     public void UpdateANN()
     {
         for (int i = 0; i < populationSize; i++)
-        {
-            cars[i].UpdateFitness();//gets bots to set their corrosponding networks fitness
-        }
+            cars[i].UpdateFitness();
+
         networks.Sort();
-        networks[populationSize - 1].Save("Assets/Save.txt");//saves networks weights and biases to file, to preserve network performance
         for (int i = 0; i < populationSize / 2; i++)
         {
             networks[i] = networks[i + populationSize / 2].copy(new NeuralNetwork(layers));
